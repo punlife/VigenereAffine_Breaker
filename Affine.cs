f8e941e553a9b883a1cd7e819cb2c7c0c0d67643
PunLife-Laptop\punlife <lolkares@gmail.com>
2019-04-11 13:09:27 +0100

    Initial commit

---
+++ Affine.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VigenereAffine_Breaker
+{
+    static class Affine
+    {
+        public static int[] valuesOfA = new int[12] { 1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25 };
+        private const string letterlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static Dictionary<char, int> populateDictionary()
+        {
+            Dictionary<char, int> charToValue = new Dictionary<char, int>();
+            for (int i = 0; i < 26; i++)
+            {
+                charToValue.Add(letterlist.ElementAt(i), i);
+            }
+            return charToValue;
+        }
+
+        public static string encrypt(string text, int a, string k, Dictionary<char, int> charToValue)
+        {
+            string cipherText = "";
+            string key = k.ToUpper();
+            string plainText = text.ToUpper();
+            int counter = 0;
+            int keyLength = key.Length;
+            while (key.Length < plainText.Length)
+            {
+                key += key.ElementAt(counter);
+                counter++;
+                if (counter <= keyLength)
+                {
+                    counter = 0;
+                }
+            }
+            for (int j = 0; j < plainText.Length; j++)
+            {
+                if (!plainText.ElementAt(j).Equals(' '))
+                {
+                    int temp = 0;
+                    int value = charToValue[plainText.ElementAt(j)];
+                    int x = 0;
+                    x = (a * value) + charToValue[key.ElementAt(j)];
+                    int b = 26;
+                    temp = ((x % b) + b) % b;
+                    cipherText += letterlist.ElementAt(temp);
+                }
+                else
+                {
+                    cipherText += " ";
+                }
+            }
+
+            return cipherText;
+        }
+        public static string encrypt(string text, int[] a, string k, Dictionary<char, int> charToValue)
+        {
+            int counter = 0;
+            int aCounter = 0;
+            string cipherText = "";
+            string key = k.ToUpper();
+            string plainText = text.ToUpper();
+            int keyLength = key.Length;
+         
+            while (key.Length < plainText.Length)
+            {
+                key += key.ElementAt(counter);
+                counter++;
+                if (counter == keyLength)
+                {
+                    counter = 0;
+                }
+            }
+            for (int j = 0; j < plainText.Length; j++)
+            {
+                if (!plainText.ElementAt(j).Equals(' '))
+                {
+                    if (a.Length == aCounter)
+                        aCounter = 0;
+                    int temp = 0;
+                    int value = charToValue[plainText.ElementAt(j)];
+                    int x = 0;
+                    x = (a[aCounter] * value) + charToValue[key.ElementAt(j)];
+                    int b = 26;
+                    temp = ((x % b) + b) % b;
+                    cipherText += letterlist.ElementAt(temp);
+                    aCounter++;
+                }
+                else
+                {
+                    cipherText += " ";
+                }
+            }
+
+            return cipherText;
+        }
+        public static string decrypt(string text, int a, string k, Dictionary<char, int> charToValue)
+        {
+            string plainText = "";
+            string key = k.ToUpper();
+            string cipherText = text.ToUpper();
+            int counter = 0;
+            int keyLength = key.Length;
+            while (key.Length < cipherText.Length)
+            {
+                key += key.ElementAt(counter);
+                counter++;
+                if (counter <= keyLength)
+                {
+                    counter = 0;
+                }
+            }
+            for (int j = 0; j < cipherText.Length; j++)
+            {
+                if (!cipherText.ElementAt(j).Equals(' '))
+                {
+                    int temp = 0;
+                    int value = charToValue[cipherText.ElementAt(j)];
+                    int x = 0;
+                    x = calculateModularMultiplicativeInverse(a) * (value - charToValue[key.ElementAt(j)]);
+                    int b = 26;
+                    temp = ((x % b) + b) % b;
+                    plainText += letterlist.ElementAt(temp);
+                }
+                else
+                {
+                    plainText += " ";
+                }
+            }
+
+            return plainText;
+        }
+        public static string decrypt(string text, int[] a, string k, Dictionary<char, int> charToValue)
+        {
+            string plainText = "";
+            string key = k.ToUpper();
+            string cipherText = text.ToUpper();
+            int counter = 0;
+            int aCounter = 0;
+            int keyLength = key.Length;
+            while (key.Length < cipherText.Length)
+            {
+                key += key.ElementAt(counter);
+                counter++;
+                if (counter == keyLength)
+                {
+                    counter = 0;
+                }
+            }
+            for (int j = 0; j < cipherText.Length; j++)
+            {
+                if (!cipherText.ElementAt(j).Equals(' '))
+                {
+                    if (a.Length == aCounter)
+                        aCounter = 0;
+                    int temp = 0;
+                    int value = 0;
+                    if (charToValue.ContainsKey(cipherText.ElementAt(j)))
+                        value = charToValue[cipherText.ElementAt(j)];
+                    else
+                        continue;
+                    int x = 0;
+                    x = calculateModularMultiplicativeInverse(a[aCounter]) * (value - charToValue[key.ElementAt(j)]);
+                    int b = 26;
+                    temp = ((x % b) + b) % b;
+                    plainText += letterlist.ElementAt(temp);
+                    aCounter++;
+                }
+                else
+                {
+                    plainText += " ";
+                }
+            }
+
+            return plainText;
+        }
+
+        public static List<string> bruteForceAB(string message, string expected)
+        {
+            Dictionary<char, int> charToValue = populateDictionary();
+            List<string> matchedCombinations = new List<string>();
+            for (int i = 0; i < valuesOfA.Length; i++)
+            {
+                for (int l = 0; l < letterlist.Length; l++)
+                {
+                    string temp = encrypt(message, valuesOfA[i], letterlist[l].ToString(), charToValue);
+                    //Console.WriteLine("A:[ " + valuesOfA[i] + "] B:[" + letterlist.ElementAt(l) + "]" + temp);
+                    if (temp.Equals(expected.ToUpper()))
+                    {                      
+                        //Console.WriteLine("^^^^EXPECTED LETTER MATCHED^^^^");
+                        matchedCombinations.Add("A[" + valuesOfA[i] + "]:B[" + letterlist.ElementAt(l) + "]");
+                    }
+                }
+            }
+            //Console.WriteLine("Brute force complete...");
+            return matchedCombinations;
+        }
+        private static int calculateModularMultiplicativeInverse(int a)
+        {
+            const int n = 26;
+            double temp = 0;
+            for (double i = 1; i != 0; i++)
+            {
+                temp = ((i * n) + 1) / a;
+                if (temp % 1 == 0)
+                    return Convert.ToInt32(temp);
+            }
+            throw new Exception();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VigenereAffine_Breaker
+{
+    class Program
+    {
+        private static Dictionary<char, int> dict = new Dictionary<char, int>();
+        private static List<List<char>> columnList = new List<List<char>>();
+        private static List<int> aKey = new List<int>();
+        private static string bKey = string.Empty;
+        private static string ciphertext = string.Empty;
+        private static int KeySize = 6;
+        static void Main(string[] args)
+        {
+            dict = Affine.populateDictionary();
+            //KeySize goes in as a parameter (can be worked out with CryptTool)
+            columnList = loadCipherText(KeySize);
+            int counter = 1;
+            for (int i = 0; i < columnList.Count; i++)
+            {
+                Console.WriteLine("---Column " + counter + " ---");
+                columnConfigBreak(columnList[i]);
+                counter++;
+            }
+            attemptDecryption(ciphertext);
+            Console.Read();
+        }
+        private static List<List<char>> loadCipherText(int _keySize)
+        {
+            ciphertext = File.ReadAllText("cipher.txt").ToUpper();
+            List<List<char>> columns = new List<List<char>>();
+            string text = ciphertext;
+            int keySize = _keySize;
+            int counter = 0;
+            for (int i = 0; i < keySize; i++)
+            {
+                columns.Add(new List<char>());
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                columns[counter].Add(text[i]);
+                if (counter < keySize-1)
+                    counter++;
+                else
+                    counter = 0;
+            }
+            return columns;
+        }
+        private static string[] findMostFrequentElementInTheArray(List<char> list)
+        {
+            Dictionary<char, int> frequencyDict = new Dictionary<char, int>();
+            char mostCommon = list[0];
+            double occurences = 0;
+            int tempOccurences = 0;
+            foreach (char character in list)
+            {
+                if (!frequencyDict.ContainsKey(character))
+                {
+                    frequencyDict.Add(character, 1);
+                }
+                else
+                {
+                    tempOccurences = frequencyDict[character] + 1;
+                    frequencyDict[character] = tempOccurences;
+                    if (occurences < tempOccurences)
+                    {
+                        occurences = tempOccurences;
+                        mostCommon = character;
+                    }
+                }
+            }
+            Console.WriteLine("_______________________________________________________________________________");
+            Console.WriteLine("The most commmon character is " + mostCommon + " And it appears " + occurences + " times out of " + list.Count + " times");
+            double percentage = (occurences / list.Count) * 100;
+            Console.WriteLine("Frequency percentage: " + percentage + "%");
+            int[] temp = frequencyDict.Values.ToArray();
+            Array.Sort(temp);
+            var myKey = frequencyDict.FirstOrDefault(x => x.Value == temp[temp.Length-2]).Key;
+            Console.WriteLine("The second most commmon character is " + myKey + " And it appears " + temp[temp.Length - 2] + " times out of " + list.Count + " times");
+            percentage = ((double)temp[temp.Length - 2] / list.Count) * 100;
+            Console.WriteLine("Frequency percentage: " + percentage + "%");
+            return new string[] { mostCommon.ToString(), myKey.ToString() };
+        }
+        private static void columnConfigBreak(List<char> collection)
+        {
+            string[] aux = findMostFrequentElementInTheArray(collection);
+            List<string> combinationsCol1 = Affine.bruteForceAB("E", aux[0]);
+            List<string> combinationsCol2 = Affine.bruteForceAB("T", aux[1]);
+            foreach (string s in combinationsCol1.Intersect(combinationsCol2))
+            {
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                Console.WriteLine("Common configuration of A:B for two most common letters is: " + s);
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                string a = s.Split(':')[0].Replace("A[","").Replace("]","");
+                string b = s.Split(':')[1].Replace("B[", "").Replace("]", "");
+                aKey.Add(Int16.Parse(a));
+                bKey += b;
+            }
+        }
+        private static void attemptDecryption(string ciphertext)
+        {
+            Console.WriteLine("-----------------------------------Decryption attempt-----------------------------------");
+            Console.WriteLine("A Key: " + string.Join(",", aKey));
+            Console.WriteLine("B Key: " + bKey);
+            Console.WriteLine(Affine.decrypt(ciphertext, aKey.ToArray(), bKey, dict));
+
+        }
+    }
+}
